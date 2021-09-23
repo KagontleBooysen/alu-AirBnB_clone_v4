0d36b8fdce818aa8f21930e1dbb1790bcc940c38
Ro8s <rodrigo.delgado3132@gmail.com>
2021-09-23 20:19:32 +0000

    added ex 2

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,24 @@
+$( document ).ready(function() {
+	let list_amenities = {};
+
+	$('.amenities .popover ul li input').click(function(e) {
+		if (list_amenities[e.target.dataset.id] === undefined)
+		{
+			list_amenities[e.target.dataset.id] = e.target.dataset.name;
+		}
+		else
+		{
+			delete list_amenities[e.target.dataset.id];
+		}
+
+		let text = "";
+		for (let elem in list_amenities) {
+			text += list_amenities[elem] + ", ";
+		}
+		
+		if (text === "")
+			$('.amenities h4').html("&nbsp;");
+		else
+			$('.amenities h4').text(text.substring(0, text.length-2));
+	});
+});
